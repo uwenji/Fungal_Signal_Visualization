d2afb1b0a3695be604e80fb07396299020046035
You-Wen Ji <yw.ji90@gmail.com>
2022-09-07 14:42:50 +0200

    again

--- ploting.py
+++
@@ -1,9 +0,0 @@
-#%%
-import plotly.express as px
-
-df = px.data.gapminder().query("country=='Canada'")
-fig = px.line(df, x="year", y="lifeExp", title='Life expectancy in Canada')
-fig.show()
-# %%
-#let push
-#danish keyBBBBBB
